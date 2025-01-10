6e7a157b2c05d2c85bdf84b5a38203022e623989
bob <qianpd22@icloud.com>
2025-01-10 13:04:00 +0000

    Fix bug Change Layout

--- src/app/SearchPage.js
+++ src/app/SearchPage.js
@@ -12,10 +12,9 @@ const SearchPage = () => {
 
     const [filters, setFilters] = useState({
         keywords: "",
-        selectedCategories: [],
+        selectedDomains: [],
         selectedTypes: [],
         selectedImageModality: [],
-        selectedShapes: [],
         selectedFileTypes: [],
         cellSize: [0, 1000],
         cellCount: [0, 1000],
@@ -24,10 +23,9 @@ const SearchPage = () => {
     const resetFilters = () => {
         setFilters({
             keywords: "",
-            selectedCategories: [],
+            selectedDomains: [],
             selectedTypes: [],
             selectedImageModality: [],
-            selectedShapes: [],
             selectedFileTypes: [],
             cellSize: [0, 1000],
             cellCount: [0, 1000],
@@ -37,7 +35,7 @@ const SearchPage = () => {
     const [viewMode, setViewMode] = useState("grid"); // Default view mode
     const [sortOrder, setSortOrder] = useState("newest"); // Default sorting order
     const [data, setData] = useState([]); // Mock database
-    const [uniqueCategories, setUniqueCategories] = useState([]);
+    const [uniqueDomains, setUniqueDomains] = useState([]);
     const [uniqueTypes, setUniqueTypes] = useState([]);
     const [uniqueImageModalities, setUniqueImageModalities] = useState([]);
     const [uniqueShapes, setUniqueShapes] = useState([]);
@@ -75,18 +73,15 @@ const SearchPage = () => {
             setData((prevData) => {
                 const updatedData = [...prevData, newEntry];
 
-                // Update unique categories, types, and other properties
-                setUniqueCategories((prevCategories) => [
-                    ...new Set([...prevCategories, newEntry.category]),
+                // Update unique domains, types, and other properties
+                setUniqueDomains((prevDomains) => [
+                    ...new Set([...prevDomains, newEntry.domain]),
                 ]);
                 setUniqueTypes((prevTypes) => [
                     ...new Set([...prevTypes, newEntry.type]),
                 ]);
                 setUniqueImageModalities((prevModalities) => [
                     ...new Set([...prevModalities, newEntry.imageModality]),
-                ]);
-                setUniqueShapes((prevShapes) => [
-                    ...new Set([...prevShapes, newEntry.shape]),
                 ]);
                 setUniqueFileTypes((prevFileTypes) => [
                     ...new Set([...prevFileTypes, newEntry.fileType]),
@@ -107,12 +102,12 @@ const SearchPage = () => {
         navigate(`/details/${id}`);
     };
 
-    const handleCategoryChange = (category) => {
+    const handleDomainChange = (domain) => {
         setFilters((prevFilters) => ({
             ...prevFilters,
-            selectedCategories: prevFilters.selectedCategories.includes(category)
-                ? prevFilters.selectedCategories.filter((item) => item !== category)
-                : [...prevFilters.selectedCategories, category],
+            selectedDomains: prevFilters.selectedDomains.includes(domain)
+                ? prevFilters.selectedDomains.filter((item) => item !== domain)
+                : [...prevFilters.selectedDomains, domain],
         }));
     }
 
@@ -131,15 +126,6 @@ const SearchPage = () => {
             selectedImageModality: prevFilters.selectedImageModality.includes(imageModality)
                 ? prevFilters.selectedImageModality.filter((item) => item !== imageModality)
                 : [...prevFilters.selectedImageModality, imageModality],
-        }));
-    }
-
-    const handleShapeChange = (shape) => {
-        setFilters((prevFilters) => ({
-            ...prevFilters,
-            selectedShapes: prevFilters.selectedShapes.includes(shape)
-                ? prevFilters.selectedShapes.filter((item) => item !== shape)
-                : [...prevFilters.selectedShapes, shape],
         }));
     }
 
@@ -169,17 +155,14 @@ const SearchPage = () => {
                 item.name.toLowerCase().includes(filters.keywords.toLowerCase())
             );
         }
-        if (filters.selectedCategories.length > 0) {
-            results = results.filter((item) => filters.selectedCategories.includes(item.category));
+        if (filters.selectedDomains.length > 0) {
+            results = results.filter((item) => filters.selectedDomains.includes(item.domain));
         }
         if (filters.selectedTypes.length > 0) {
             results = results.filter((item) => filters.selectedTypes.includes(item.type));
         }
         if (filters.selectedImageModality.length > 0) {
             results = results.filter((item) => filters.selectedImageModality.includes(item.imageModality));
-        }
-        if (filters.selectedShapes.length > 0) {
-            results = results.filter((item) => filters.selectedShapes.includes(item.shape));
         }
         if (filters.selectedFileTypes.length > 0) {
             results = results.filter((item) => filters.selectedFileTypes.includes(item.fileType));
@@ -214,7 +197,7 @@ const SearchPage = () => {
     // Inline SVGs for toggle button
     const ExpandIcon = (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd"
+            <path fillRule="evenodd" clipRule="evenodd"
                   d="M8.85719 3H15.1428C16.2266 2.99999 17.1007 2.99998 17.8086 3.05782C18.5375 3.11737 19.1777 3.24318 19.77 3.54497C20.7108 4.02433 21.4757 4.78924 21.955 5.73005C22.2568 6.32234 22.3826 6.96253 22.4422 7.69138C22.5 8.39925 22.5 9.27339 22.5 10.3572V13.6428C22.5 14.7266 22.5 15.6008 22.4422 16.3086C22.3826 17.0375 22.2568 17.6777 21.955 18.27C21.4757 19.2108 20.7108 19.9757 19.77 20.455C19.1777 20.7568 18.5375 20.8826 17.8086 20.9422C17.1008 21 16.2266 21 15.1428 21H8.85717C7.77339 21 6.89925 21 6.19138 20.9422C5.46253 20.8826 4.82234 20.7568 4.23005 20.455C3.28924 19.9757 2.52433 19.2108 2.04497 18.27C1.74318 17.6777 1.61737 17.0375 1.55782 16.3086C1.49998 15.6007 1.49999 14.7266 1.5 13.6428V10.3572C1.49999 9.27341 1.49998 8.39926 1.55782 7.69138C1.61737 6.96253 1.74318 6.32234 2.04497 5.73005C2.52433 4.78924 3.28924 4.02433 4.23005 3.54497C4.82234 3.24318 5.46253 3.11737 6.19138 3.05782C6.89926 2.99998 7.77341 2.99999 8.85719 3ZM6.35424 5.05118C5.74907 5.10062 5.40138 5.19279 5.13803 5.32698C4.57354 5.6146 4.1146 6.07354 3.82698 6.63803C3.69279 6.90138 3.60062 7.24907 3.55118 7.85424C3.50078 8.47108 3.5 9.26339 3.5 10.4V13.6C3.5 14.7366 3.50078 15.5289 3.55118 16.1458C3.60062 16.7509 3.69279 17.0986 3.82698 17.362C4.1146 17.9265 4.57354 18.3854 5.13803 18.673C5.40138 18.8072 5.74907 18.8994 6.35424 18.9488C6.97108 18.9992 7.76339 19 8.9 19H9.5V5H8.9C7.76339 5 6.97108 5.00078 6.35424 5.05118ZM11.5 5V19H15.1C16.2366 19 17.0289 18.9992 17.6458 18.9488C18.2509 18.8994 18.5986 18.8072 18.862 18.673C19.4265 18.3854 19.8854 17.9265 20.173 17.362C20.3072 17.0986 20.3994 16.7509 20.4488 16.1458C20.4992 15.5289 20.5 14.7366 20.5 13.6V10.4C20.5 9.26339 20.4992 8.47108 20.4488 7.85424C20.3994 7.24907 20.3072 6.90138 20.173 6.63803C19.8854 6.07354 19.4265 5.6146 18.862 5.32698C18.5986 5.19279 18.2509 5.10062 17.6458 5.05118C17.0289 5.00078 16.2366 5 15.1 5H11.5ZM5 8.5C5 7.94772 5.44772 7.5 6 7.5H7C7.55229 7.5 8 7.94772 8 8.5C8 9.05229 7.55229 9.5 7 9.5H6C5.44772 9.5 5 9.05229 5 8.5ZM5 12C5 11.4477 5.44772 11 6 11H7C7.55229 11 8 11.4477 8 12C8 12.5523 7.55229 13 7 13H6C5.44772 13 5 12.5523 5 12Z"
                   fill="currentColor"></path>
         </svg>
@@ -233,23 +216,23 @@ const SearchPage = () => {
 
                 {/* Existing Sidebar Content */}
                 <div className="filters">
-                    {/* Categories */}
+                    {/* Domains */}
                     <div className="filter-section">
-                        <h3>Categories</h3>
-                        {uniqueCategories.map((category) => (
-                            <label key={category} className="vertical-checkbox">
+                        <h3>Domains</h3>
+                        {uniqueDomains.map((domain) => (
+                            <label key={domain} className="vertical-checkbox">
                                 <input
                                     type="checkbox"
-                                    checked={filters.selectedCategories.includes(category)}
-                                    onChange={() => handleCategoryChange(category)}
+                                    checked={filters.selectedDomains.includes(domain)}
+                                    onChange={() => handleDomainChange(domain)}
                                 />
-                                {category}
+                                {domain}
                             </label>
                         ))}
                     </div>
                     {/* Types */}
                     <div className="filter-section">
-                        <h3>Types</h3>
+                        <h3>Cell Types</h3>
                         {uniqueTypes.map((type) => (
                             <label key={type} className="vertical-checkbox">
                                 <input
@@ -275,43 +258,31 @@ const SearchPage = () => {
                             </label>
                         ))}
                     </div>
-                    {/* Shapes */}
-                    <div className="filter-section">
-                        <h3>Shapes</h3>
-                        {uniqueShapes.map((shape) => (
-                            <label key={shape} className="vertical-checkbox">
-                                <input
-                                    type="checkbox"
-                                    checked={filters.selectedShapes.includes(shape)}
-                                    onChange={() => handleShapeChange(shape)}
-                                />
-                                {shape}
-                            </label>
-                        ))}
-                    </div>
                     {/* Cell Count */}
-                    <div className="cell-count">
+                    <div className="filter-section">
                         <h3>Cell Count</h3>
-                        <div className="range-display">
-                            <span>{filters.cellCount[0]}</span>
-                            <span>{filters.cellCount[1]}</span>
+                        <div className="cell-count">
+                            <div className="range-display">
+                                <span>{filters.cellCount[0]}</span>
+                                <span>{filters.cellCount[1]}</span>
+                            </div>
+                            <Slider
+                                range
+                                min={0}
+                                max={1000}
+                                value={filters.cellCount}
+                                onChange={(value) => setFilters({...filters, cellCount: value})}
+                            />
                         </div>
-                        <Slider
-                            range
-                            min={0}
-                            max={1000}
-                            value={filters.cellCount}
-                            onChange={(value) => setFilters({ ...filters, cellCount: value })}
-                        />
                     </div>
                     {/* Reset Filters Button */}
                     <button className="reset-button" onClick={resetFilters}>
                         Reset Filters
                     </button>
+                    </div>
                 </div>
-            </div>
 
-            {/* Main Content */}
+                {/* Main Content */}
             <div className="main-container">
                 <div className="results-header">
                     <input
@@ -370,7 +341,7 @@ const SearchPage = () => {
                                     >
                                         <img src={result.imageUrl} alt={result.name} />
                                         <p>{result.name}</p>
-                                        <p>{result.category}</p>
+                                        <p>{result.domain}</p>
                                     </div>
                                 ) : (
                                     <div
@@ -385,7 +356,7 @@ const SearchPage = () => {
                                         />
                                         <div className="result-details">
                                             <h4>{result.name}</h4>
-                                            <p>{result.category}</p>
+                                            <p>{result.domain}</p>
                                         </div>
                                     </div>
                                 )
@@ -399,3 +370,4 @@ const SearchPage = () => {
 };
 
 export default SearchPage;
+
