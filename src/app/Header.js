37a6d649d5c91d5f0f74a92b63619cc72d4c31aa
weronika-w <weronika.wakula@gmail.com>
2025-01-11 23:08:10 +0000

    Added home navigation button

--- src/app/Header.js
+++ src/app/Header.js
@@ -20,6 +20,7 @@ const Header = () => {
                 />
             </div>
             <nav className="nav-links">
+                <a onClick={() => navigate("")}>Home</a>
                 <a onClick={() => navigate("/user")}>My Profile</a>
                 <a onClick={() => navigate("/help")}>Help</a>
             </nav>
