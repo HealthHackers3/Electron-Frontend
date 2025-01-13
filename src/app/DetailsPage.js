0c696e80635e12c3181d75a03e81a400b487dfbf
Sam Clifford <sc2522@ic.ac.uk>
2025-01-13 23:19:51 +0000

    fetch post fields API

--- src/app/DetailsPage.js
+++ src/app/DetailsPage.js
@@ -1,11 +1,28 @@
 import "./DetailsPage.css";
 import React from "react";
+import '@fortawesome/fontawesome-free/css/all.css';
 import { useParams, useLocation, useNavigate } from "react-router-dom";
 
 const DetailsPage = () => {
-    const { id } = useParams();
+    const HeartIcon = () => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="heart-icon"
+        >
+            <path
+                d="M352.92 80C288 80 256 144 256 144s-32-64-96.92-64c-52.76 0-94.54 44.14-95.08 96.81-1.1 109.33 86.73 187.08 183 252.42a16 16 0 0018 0c96.26-65.34 184.09-143.09 183-252.42-.54-52.67-42.32-96.81-95.08-96.81z"
+                ></path>
+        </svg>
+    );
+    const {id} = useParams();
     const location = useLocation();
-    const { imageUrl, properties, parent } = location.state || {};
+    const {imageUrl, properties, parent} = location.state || {};
 
     const handleDownload = async () => {
         try {
@@ -72,6 +89,37 @@ const DetailsPage = () => {
                         <li><strong>Image Modality:</strong> {properties?.imageModality || "N/A"}</li>
                         <li><strong>Author:</strong> {properties?.author || "Unknown"}</li>
                     </ul>
+                    <button className="btn-love">
+                        <span className="fa fa-heart"></span>
+                        <div className="small-ornament">
+                            <div className="ornament o-1"></div>
+                            <div className="ornament o-2"></div>
+                            <div className="ornament o-3"></div>
+                            <div className="ornament o-4"></div>
+                            <div className="ornament o-5"></div>
+                            <div className="ornament o-6"></div>
+                        </div>
+                        <div className="circle">
+                            <svg>
+                                <ellipse
+                                    id="eclipse"
+                                    rx="50"
+                                    ry="50"
+                                    cx="67.5"
+                                    cy="67.5"
+                                    fillOpacity="1"
+                                    strokeLinecap=""
+                                    strokeDashoffset=""
+                                    fill="transparent"
+                                    strokeDasharray=""
+                                    strokeOpacity="1"
+                                    strokeWidth="0"
+                                    stroke="#988ADE"
+                                ></ellipse>
+                            </svg>
+                        </div>
+                    </button>
+
                 </div>
             </div>
 
@@ -83,7 +131,7 @@ const DetailsPage = () => {
                     height="24"
                     fill="currentColor"
                 >
-                    <path d="M10 19l-7-7 7-7v4h8v6h-8v4z" />
+                    <path d="M10 19l-7-7 7-7v4h8v6h-8v4z"/>
                 </svg>
                 <span>Back</span>
             </div>
